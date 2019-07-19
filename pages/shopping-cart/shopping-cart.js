300706e8664262ff365097065d5af28a59648b7a
LY <coderyliu@gmail.com>
2019-07-20 00:02:08 +0800

    购物车模块

--- pages/shopping-cart/shopping-cart.js
+++ pages/shopping-cart/shopping-cart.js
@@ -1,18 +1,50 @@
 // pages/shopping-cart/shopping-cart.js
+
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    cartList: [],
+    isSelectAll: true,
+    totalPrice: 0,
+    totalCounter: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      // 第一次加载数据
+      cartList: app.globalData.cartList
+    })
 
+    // 设置回调
+    app.addCartCallback = () => {
+      this.setData({
+        cartList: app.globalData.cartList
+      })
+      this.changeData()
+    }
+
+    // 设置修改某个商品的回调
+    app.changeGoodsState = (index, goods) => {
+      // 修改某一项的选中状态
+      this.setData({
+        [`cartList[${index}]`]: goods
+      })
+
+      // 修改全部选中的状态
+      const selectAll = !this.data.cartList.find(item => !item.checked)
+      this.setData({
+        isSelectAll: selectAll
+      })
+      this.changeData()
+    }
   },
 
   /**
@@ -26,7 +58,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    wx.setNavigationBarTitle({
+      title: `购物车(${this.data.cartList.length})`,
+    })
 
+    this.changeData()
   },
 
   /**
@@ -62,5 +98,48 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onSelectAll() {
+    // 判断是否是全部选中
+    if (this.data.isSelectAll) { // 目前全部选中
+      this.data.cartList.forEach(item => {
+        item.checked = false
+      })
+      this.setData({
+        cartList: this.data.cartList,
+        isSelectAll: false
+      })
+    } else { // 某些没选中
+      this.data.cartList.forEach(item => {
+        item.checked = true
+      })
+      this.setData({
+        cartList: this.data.cartList,
+        isSelectAll: true
+      })
+    }
+  },
+
+  changeData() {
+    // 获取所有选中数据
+    let totalPrice = 0;
+    let counter = 0;
+
+    for (let item of this.data.cartList) {
+      if (item.checked) {
+        counter++
+        totalPrice += item.price * item.count
+      }
+    }
+
+    // console.log(counter, totalPrice)
+
+    // 修改数据
+    this.setData({
+      totalCounter: counter,
+      totalPrice: totalPrice
+    })
   }
+  
 })
